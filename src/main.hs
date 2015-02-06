6696fa3e3c188135e4a3065cc911ea5406496cdf
Gordon Stewart <gstew5@gmail.com>
2015-02-05 22:50:43 -0500

    Examples

--- src/main.hs
+++ src/main.hs
@@ -16,13 +16,37 @@ import Prelude hiding
 import qualified Prelude as P  
 
 import Syntax
+import Lang
 
-g = do { x <- var
+-- 1. a standalone "program" in the expression language
+prog1 
+  = do { x <- var
        ; y <- var
        ; z <- var
        ; u <- ret (if x + y then y else z)
+       ; v <- ret (if z then x else y)
        ; w <- ret (if x then y else z)
-       ; ret $ x + u*z - w }
+       ; ret $ x + u*z - w*u*u*y*y*v }
 
-main = putStrLn $ show $ check g [0,1,1]
+-- 2. we can also mix Haskell code with R1CS expressions, by defining
+-- combinators over R1CS-producing functions.
+
+-- for example, the following code calculates the R1CS expression
+--   n + n-1 + n-2 + ... + n-(n-1) + x
+-- with x an input variable.
+prog2 n
+  = do { x <- var
+       ; let f i = exp_of_int i + x
+       ; ret $ summate n f }
+
+-- 3. and this little program sums over an "array"...
+prog3 
+  = do { x <- var
+       ; a <- arr 5                       -- an array of size 5
+       ; y <- ret (summate 5 (arr_get a)) -- sum the elements of the array
+       ; ret $ y - x  }                   -- and output the result, minus x
+
+do_check prog w = putStrLn $ show $ check prog w
+
+main = do_check prog3 [1,1,2,3,4,5]
 
--- src/syntax.hs
+++ src/syntax.hs
@@ -31,6 +31,15 @@ runState mf s = case mf of
 var :: State Int (Exp Rational)
 var = State (\s -> (EVar s,(P.+) s 1))
 
+arr :: Int -> State Int (Exp Rational)
+arr 0 = error "array must have size >= 1"
+arr 1 = var
+arr n = do { x <- var; _ <- arr ((P.-) n 1); ret x }
+
+arr_get :: Exp Rational -> Int -> Exp Rational
+arr_get (EVar x) n = EVar $ (P.+) x n
+arr_get _ _ = error "expected a variable"
+
 (>>=) :: State s a -> (a -> State s b) -> State s b
 (>>=) mf g
   = State (\s -> let (e,s') = runState mf s
@@ -55,10 +64,30 @@ ret = return
 
 fromRational r = EVal (r :: Rational)
 
+exp_of_int :: Int -> Exp Rational
+exp_of_int i = EVal (fromIntegral i)
+
+iter :: Int
+     -> (Int -> Exp Rational -> Exp Rational)
+     -> Exp Rational
+     -> Exp Rational
+iter 0 f e = f 0 e
+iter n f e = f n $ iter ((P.-) n 1) f e
+
+summate :: Int
+        -> (Int -> Exp Rational)
+        -> Exp Rational
+summate n f
+  = iter ((P.-) n 1)
+    (\i e -> case i == 0 of
+        True -> e
+        False -> f i + e) (f (0::Int))
+
 data Result = 
   Result { sat :: Bool
          , vars :: Int
-         , constraints :: Int }
+         , constraints :: Int
+         , result :: Rational }
   deriving Show  
 
 check :: State Int (Exp Rational) -> [Rational] -> Result
@@ -67,4 +96,6 @@ check mf inputs
         (f,r1cs) = compile_exp e 
         nw = num_vars r1cs
         ng = num_constraints r1cs
-    in Result (sat_r1cs (f inputs) r1cs) nw ng
+        wit = f inputs
+        out = head $ drop (length inputs) wit 
+    in Result (sat_r1cs wit r1cs) nw ng out
