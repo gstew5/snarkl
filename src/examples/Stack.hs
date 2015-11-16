52e67c3a80a1171c95a7f1fcc3355811f722f547
Gordon Stewart <gstew5@gmail.com>
2015-11-16 11:43:16 -0500

    Stacks (implemented by Logan and Sam)

---
+++ src/examples/Stack.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE RebindableSyntax
+           , DataKinds
+  #-}
+
+module Stack where
+
+import Prelude hiding 
+  ( (>>)
+  , (>>=)
+  , (+)
+  , (-)    
+  , (*)    
+  , (/)
+  , (&&)        
+  , return
+  , fromRational
+  , negate    
+  )
+
+import Data.Typeable
+
+import SyntaxMonad
+import Syntax
+import TExpr
+import Toplevel
+import List
+
+type TStack a = TList a
+
+type Stack a = TExp (TStack a) Rational
+
+empty_stack :: Typeable a => Comp (TStack a)
+empty_stack = nil
+
+push_stack :: Typeable a => TExp a Rational -> Stack a -> Comp (TStack a)
+push_stack p q = cons p q
+
+pop_stack :: Typeable a => Stack a -> Comp (TStack a)
+pop_stack f = tail_list f
+
+top_stack :: Typeable a => TExp a Rational-> Stack a -> Comp  a
+top_stack def e = head_list def e
+
+  
